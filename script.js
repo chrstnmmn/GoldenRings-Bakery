5e0d682403c0794302c5a9aaa1b3344e0ae70b57
Christian Emmanuel <christianemmanuelcollo23@gmail.com>
2024-07-01 11:27:53 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// When the button "Order now" is clicked the website will go to the donut shop section
+function go_to_Shop() {
+	window.location.href = "./shop.html";
+}
+
+// Go to facebook.com when the button is clicked
+function go_to_facebook() {
+	window.location.href = "https://www.facebook.com/";
+}
+
+// Go to instagram.com when the button is clicked
+function go_to_instagram() {
+	window.location.href = "https://www.instagram.com/";
+}
+
+// Go to x.com when the button is clicked
+function go_to_twitter() {
+	window.location.href = "https://x.com/?lang=en";
+}
